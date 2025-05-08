5309901346f3157ab37aae9637e4f64aa2abd498
Zhangyao719 <1258858403@qq.com>
2025-05-08 23:55:20 +0800

    feat: Tomcat 与 Servlet

---
+++ project02/src/main/java/com/servlet/ServletDemo.java
@@ -0,0 +1,20 @@
+package com.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/hello")
+public class ServletDemo extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 1. 获取请求参数值
+        String name = req.getParameter("name");
+        // 2. 响应结果
+        resp.setContentType("text/plain;charset=UTF-8");
+        resp.getWriter().write("hello, " + name);
+    }
+}
